518e4916f56a1a0f82c91b26959857a46f26572d
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-12 16:06:51 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">
+    <div className=' w-full h-screen bg-[#0F172A] text-white'>
+      <h1 className="text-3xl">
         Hello
       </h1>
     </div>
