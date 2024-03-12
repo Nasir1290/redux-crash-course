4e2ff0bdac74cebbbd73a30c4d5ae328ed1387bf
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-12 19:59:23 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import Count from './components/Count'
+import React, { useState } from "react";
+import Count from "./components/Count";
 
+const initialCounters = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+];
 const App = () => {
+  const [counters, setCounters] = useState(initialCounters);
+
+  const handleIncrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return { ...counter, value: counter.value + 1 };
+      }
+      return counter;
+    });
+
+    setCounters(updatedCounters);
+  };
+
+  const handleDecrement = (counterId) => {
+    const updatedCounters = counters.map((counter) => {
+      if (counter.id === counterId) {
+        return counter.value>=1? { ...counter, value: counter.value - 1 }:{...counter,value:0};
+      }
+      return counter;
+    });
+
+    setCounters(updatedCounters);
+  };
+
   return (
     <div>
-      <Count/>
+      {counters.map((counter) => (
+        <Count
+          key={counter.id}
+          count={counter.value}
+          onIncrement={() => handleIncrement(counter.id)}
+          onDecrement={() => handleDecrement(counter.id)}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-export default function Count() {
+export default function Count({ count, onIncrement, onDecrement }) {
   return (
-    <div>
-      <div className=" bg-slate-400 w-min p-4 rounded-lg m-5 text-white text-lg flex items-center justify-center flex-col font-bold">
-        <h1>Count:0</h1>
-        <button className="text-white bg-black p-3 rounded-md border">
+    <div className="bg-purple-400 w-min p-4 rounded-lg m-5 text-white text-lg flex flex-col items-center justify-center gap-2 font-bold">
+      <div>
+        <h1>Count:{count}</h1>
+      </div>
+      <div className=" flex gap-2">
+        <button
+          onClick={onIncrement}
+          className="text-white bg-green-500 p-3 rounded-md border"
+        >
           Increment
         </button>
-      </div>
-      <div className=" bg-purple-400 w-min p-4 rounded-lg m-5 text-white text-lg flex items-center justify-center flex-col font-bold">
-        <h1>Count:0</h1>
-        <button className="text-white bg-red-500 p-3 rounded-md border">
+        <button
+          onClick={onDecrement}
+          className="text-white bg-red-500 p-3 rounded-md border"
+        >
           Decrement
         </button>
       </div>
