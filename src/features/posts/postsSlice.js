3ee270aa635ae212c44a2348f85242a20d74c739
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-14 09:35:20 +0600

    add some code

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
 const initialState = {
     posts: [],
     isLoading: false,
     isError: false,
     error: null
 }
+
+const postsSlice = createSlice({
+    name:"posts",
+    initialState:initialState,
+    reducers:{
+
+    }
+})
+
+export default postsSlice.reducer;
